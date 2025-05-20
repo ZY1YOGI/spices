cded5f2e0fdc6c1266989e50906350ad68f3572c
Youssef Amjad <zy1yogi@gmail.com>
2025-05-21 00:55:58 +0300

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
+import { resolve } from 'path'
+
 export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        about: resolve(__dirname, 'src/about-us.html'),
+        contact: resolve(__dirname, 'src/contact-us.html'),
+      },
+    }
+  },
   plugins: [
     tailwindcss(),
-  ],
+  ]
 })
